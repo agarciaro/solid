608dcb5f5430d349fea17765dbdd8d1518a7398b
agarciaro <aitorgarciarojado@gmail.com>
2024-10-25 17:58:00 +0200

    Clean Code

---
+++ CleanCode/introduccion-clean-code/introduccion-1/src/main/java/com/imagina/cleancode/Calculadora.java
@@ -0,0 +1,10 @@
+package com.imagina.cleancode;
+
+public class Calculadora {
+	
+	public Double calcular(Double a, Double b) {
+		Double d = Math.sqrt(a*a - b*b); 
+		
+		return b*d/2;
+	}
+}
---
+++ CleanCode/unit-tests/src/main/java/com/imagina/cleancode/DivisionByZeroException.java
@@ -0,0 +1,8 @@
+package com.imagina.cleancode;
+
+public class DivisionByZeroException extends RuntimeException {
+
+    public DivisionByZeroException(String message) {
+        super(message);
+    }
+}
